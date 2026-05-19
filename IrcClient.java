e4d5bdaaa58f15a8528f0e0f2508872822fa451b
TaZzerNada <eimantaszajanckovskis@gmail.com>
2026-05-19 04:15:57 +0300

    From now user can write and send messages to server channel and quit

--- IrcClient.java
+++ IrcClient.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class IrcClient {
     
@@ -10,9 +11,11 @@ public class IrcClient {
     {
         Socket socket = new Socket("irc.libera.chat", 6667);
 
+
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(socket.getInputStream())
         );
+
 
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
@@ -22,6 +25,23 @@ public class IrcClient {
         Thread.sleep(5000);
         out.println("JOIN #test");
 
+
+        Scanner scanner = new Scanner(System.in);
+
+        Thread inputThread = new Thread(() -> {
+        while (true) {
+        String message = scanner.nextLine();
+
+        if (message.equalsIgnoreCase("/quit")) {
+            out.println("QUIT :Goodbye");
+            break;
+        }
+
+        out.println("PRIVMSG #test :" + message);
+    }
+});
+
+inputThread.start();
         String line;
         while((line = in.readLine()) != null) {
             System.out.println(line);
